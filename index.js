4fc16770e6b0e07bd384ab8fd616e8ed62629723
dellis2cs <aprules68@yahoo.com>
2024-08-13 12:59:40 -0700

    add random rgb(TOP challenge)

--- index.js
+++ index.js
@@ -7,22 +7,41 @@ let greenColor = document.querySelector(".greenColor");
 let redColor = document.querySelector(".redColor");
 let blueColor = document.querySelector(".blueColor");
 let blackColor = document.querySelector(".blackColor");
+let rainbow = document.querySelector(".rainbow");
 let color = "black";
+let isRainbow = false;
+
+function rainbowColor() {
+  const randomBetween = (min, max) =>
+    min + Math.floor(Math.random() * (max - min + 1));
+  const r = randomBetween(0, 255);
+  const g = randomBetween(0, 255);
+  const b = randomBetween(0, 255);
+  return `rgb(${r},${g},${b})`;
+}
 
 greenColor.addEventListener("click", function () {
   color = "#6a9a1f";
+  isRainbow = false;
 });
 
 redColor.addEventListener("click", function () {
   color = "#d43f3f";
+  isRainbow = false;
 });
 
 blueColor.addEventListener("click", function () {
   color = "#00ace9";
+  isRainbow = false;
 });
 
 blackColor.addEventListener("click", function () {
   color = "#000000";
+  isRainbow = false;
+});
+
+rainbow.addEventListener("click", function () {
+  isRainbow = true;
 });
 
 function createGrid(grid_dim) {
@@ -36,8 +55,13 @@ function createGrid(grid_dim) {
     newDiv.style.width = `${cellSize}px`;
     newDiv.style.height = `${cellSize}px`;
     newDiv.style.textAlign = "center";
-
+    console.log(isRainbow);
+    console.log(color);
     newDiv.addEventListener("mouseover", function () {
+      if (isRainbow == true) {
+        color = rainbowColor();
+        newDiv.style.backgroundColor = `${color}`;
+      }
       newDiv.style.backgroundColor = `${color}`;
     });
 
